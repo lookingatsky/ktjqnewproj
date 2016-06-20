a6a37ee7e20f43d1783e7ee07659778c2653e6f6
lookingatsky <lookingatsky@126.com>
2016-06-20 15:37:59 +0800

    提交bug

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -43,7 +43,8 @@ class Welcome extends Front_Controller {
 			fb($inviteUid);
 			$this->db->where("id",$inviteUid);
 			$query = $this->db->get('bulk_standard',1,0);
-			fb($query);
+			$row = $query->row_array();
+			fb($query['mobile']);
 		}
 		
 		$siteinfo = $this->_siteinfo();  //获取网站信息
