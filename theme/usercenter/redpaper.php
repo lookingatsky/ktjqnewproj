5b4d910c37b47cb064a41a701180af00bb9491bb
qiner <qinningyu68@163.com>
2016-06-23 15:43:35 +0800

    第二次修改https微博分享

--- theme/usercenter/redpaper.php
+++ theme/usercenter/redpaper.php
@@ -21,13 +21,7 @@
     <script src="<?php echo base_url();?>style/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url();?>style/js/base.js"></script>
 	<script>
-	$(function(){
-		$(".weiboShare").click(function(){
-			window.open('http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=file%3A%2F%2F%2FC%3A%2FUsers%2FAdministrator%2FDesktop%2F%25E6%2596%25B0%25E5%25BB%25BA%25E6%2596%2587%25E4%25BB%25B6%25E5%25A4%25B9%2F1.html%23%230-qzone-1-14757-d020d2d2a4e8d1a374a433f596ad1440&title=&desc=&summary=&site=');
-			
-		})
-		
-	})
+	
 	</script>
 </head>
 <body>
@@ -221,9 +215,7 @@ body{
 												</button>
 											</div>
 											<div class="col-md-6 col-xs-6">
-												<button type="button" class="btn btn-info weiboShare">
-													&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;微博分享&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-												</button>
+												<button href=”javascript:(function(){window.open(‘http://v.t.sina.com.cn/share/share.php?title=’+encodeURIComponent(document.title)+’&url=’+encodeURIComponent(location.href)+’&source=bookmark’,'_blank’,'width=450,height=400′);})()” title=”新浪微博分享”><img src=”http://t.sina.com.cn/favicon.ico”  alt=”新浪微博分享” border=”0〃>分享到新浪微博</button>
 											</div>
 										</div>
 									</div>
